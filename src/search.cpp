99a992743d90c010bef8c2bfa6ba2aff4f8ecf3d
Günther Demetz <guenther.demetz@hotmail.com>
2016-08-15 12:17:14 +0200

    Move sequence history based on to squares
    bench: 6410150

--- src/search.cpp
+++ src/search.cpp
@@ -958,7 +958,6 @@ moves_loop: // When in check search starts from here
       // Step 14. Make the move
       pos.do_move(move, st, givesCheck);
 
-      bool capt = pos.captured_piece_type();
 
       // Step 15. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
@@ -972,7 +971,7 @@ moves_loop: // When in check search starts from here
                      +    (fmh  ? (*fmh )[moved_piece][to_sq(move)] : VALUE_ZERO)
                      +    (fmh2 ? (*fmh2)[moved_piece][to_sq(move)] : VALUE_ZERO)
                      +    thisThread->fromTo.get(~pos.side_to_move(), move)
-                     + ((capt && is_ok((ss-2)->currentMove)) ? pos.this_thread()->moveSeq.get(~pos.side_to_move(), to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(move)) : VALUE_ZERO);
+                     + ((cmh && fmh) ? pos.this_thread()->moveSeq.get(to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(move)) : VALUE_ZERO);
 
 
           // Increase reduction for cut nodes
@@ -1426,8 +1425,8 @@ moves_loop: // When in check search starts from here
     if (fmh2)
         fmh2->update(pos.moved_piece(move), to_sq(move), bonus);
 
-    if (pos.captured_piece_type() && is_ok((ss-2)->currentMove))
-      thisThread->moveSeq.update(c, to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(move), bonus);
+    if (cmh && fmh)
+      thisThread->moveSeq.update(to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(move), bonus);
 
 
     // Decrease all the other played quiet moves
@@ -1445,8 +1444,8 @@ moves_loop: // When in check search starts from here
         if (fmh2)
             fmh2->update(pos.moved_piece(quiets[i]), to_sq(quiets[i]), -bonus);
 
-        if (pos.captured_piece_type() && is_ok((ss-2)->currentMove))
-              thisThread->moveSeq.update(c, to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(quiets[i]), -bonus);
+        if (cmh && fmh)
+            thisThread->moveSeq.update(to_sq((ss-2)->currentMove), to_sq((ss-1)->currentMove), to_sq(quiets[i]), -bonus);
     }
 
     // Extra penalty for a quiet TT move in previous ply when it gets refuted
