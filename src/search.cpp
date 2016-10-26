e57a4a77ca5b5f163b78ef5f95b081878bc289f3
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2016-10-26 09:49:20 +0200

    Tweak rHist value from 20000 to 17000
    bench: 5607680

--- src/search.cpp
+++ src/search.cpp
@@ -1010,7 +1010,7 @@ moves_loop: // When in check search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history
-              r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->history / 20000) * ONE_PLY);
+              r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->history / 17000) * ONE_PLY);
           }
 
           Depth d = std::max(newDepth - r, ONE_PLY);
