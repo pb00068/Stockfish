da1a3c9f81cb49730ee16321a938327b107ba61b
pb00067 <guenther.demetz@hotmail.com>
2016-12-05 09:44:26 +0100

    Verification search rework
    increase reduction but disable nmp for the the first half of the search
    tree, trigger verification search rework earlier
    bench: 5620406

--- src/search.cpp
+++ src/search.cpp
@@ -771,7 +771,7 @@ namespace {
             if (nullValue >= VALUE_MATE_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (depth < 10 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
+            if (depth < 6 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
                 return nullValue;
 
             // Do verification search at high depths
