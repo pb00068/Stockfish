545914bb283fb05b8017c02e25963a725d0eb56e
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-03-01 11:10:30 +0100

    Use bestMove capture with negative SEE as good-capture in sibling nodes.
    bench: 6758862

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -192,18 +192,11 @@ Move MovePicker::next_move() {
       score<CAPTURES>();
       ++stage;
 
-//      move = ss->badcaptureKiller;  // capture killer move
-//      if(   move != MOVE_NONE
-//         && move != ttMove
-//         && pos.pseudo_legal(move)
-//         && pos.capture_or_promotion(move))
-//          return move;
-
   case GOOD_CAPTURES:
       while (cur < endMoves)
       {
           move = pick_best(cur++, endMoves);
-          if (move != ttMove) // && move != ss->badcaptureKiller)
+          if (move != ttMove)
           {
               if (move == ss->badcaptureKiller || pos.see_ge(move, VALUE_ZERO))
                   return move;
