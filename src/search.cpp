6bec7949f389675eb2723c9163e5a58cf76ca7f8
Günther Demetz <guenther.demetz@hotmail.com>
2017-03-03 22:53:28 +0100

    Take 4: When a bad-capture becomes bestMove, use it as good-capture in
    sibling nodes (only real siblings nodes this time).
    
    bench: 6006912

--- src/search.cpp
+++ src/search.cpp
@@ -602,7 +602,7 @@ namespace {
 
     ss->currentMove = (ss+1)->excludedMove = bestMove = MOVE_NONE;
     ss->counterMoves = nullptr;
-    (ss+2)->killers[0] = (ss+2)->killers[1] = (ss+2)->badcaptureKiller = MOVE_NONE;
+    (ss+2)->killers[0] = (ss+2)->killers[1] = (ss+1)->badcaptureKiller = MOVE_NONE;
     Square prevSq = to_sq((ss-1)->currentMove);
 
     // Step 4. Transposition table lookup. We don't want the score of a partial
@@ -630,8 +630,6 @@ namespace {
             {
                 if (!pos.capture_or_promotion(ttMove))
                     update_stats(pos, ss, ttMove, nullptr, 0, stat_bonus(depth));
-                else if (!pos.see_ge(ttMove, VALUE_ZERO))
-                    ss->badcaptureKiller = ttMove;
 
                 // Extra penalty for a quiet TT move in previous ply when it gets refuted
                 if ((ss-1)->moveCount == 1 && !pos.captured_piece())
@@ -1114,7 +1112,7 @@ moves_loop: // When in check search starts from here
         // Quiet best move: update move sorting heuristics
         if (!pos.capture_or_promotion(bestMove))
             update_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
-        else if (quietCount > 4 || !pos.see_ge(bestMove, VALUE_ZERO))
+        else if (quietCount > 4)
         	ss->badcaptureKiller = bestMove;
 
 
