bcb81bb75f9965de1ccf29be91c70f49a7924f2f
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-03-06 09:24:04 +0100

    Sacrifice one bit in TT-generation for arbitrary other purpose
    bench: 5803228

--- src/tt.h
+++ src/tt.h
@@ -30,7 +30,8 @@
 /// move       16 bit
 /// value      16 bit
 /// eval value 16 bit
-/// generation  6 bit
+/// generation  5 bit
+/// general use 1 bit
 /// bound type  2 bit
 /// depth       8 bit
 
@@ -40,9 +41,10 @@ struct TTEntry {
   Value value() const { return (Value)value16; }
   Value eval()  const { return (Value)eval16; }
   Depth depth() const { return (Depth)(depth8 * int(ONE_PLY)); }
+  uint8_t spc() const { return (genBound8 & 0x4); }
   Bound bound() const { return (Bound)(genBound8 & 0x3); }
 
-  void save(Key k, Value v, Bound b, Depth d, Move m, Value ev, uint8_t g) {
+  void save(Key k, Value v, Bound b, Depth d, Move m, Value ev, uint8_t g, uint8_t special) {
 
     assert(d / ONE_PLY * ONE_PLY == d);
 
@@ -53,13 +55,13 @@ struct TTEntry {
     // Don't overwrite more valuable entries
     if (  (k >> 48) != key16
         || d / ONE_PLY > depth8 - 4
-     /* || g != (genBound8 & 0xFC) // Matching non-zero keys are already refreshed by probe() */
+     /* || g != (genBound8 & 0xF8) // Matching non-zero keys are already refreshed by probe() */
         || b == BOUND_EXACT)
     {
         key16     = (uint16_t)(k >> 48);
         value16   = (int16_t)v;
         eval16    = (int16_t)ev;
-        genBound8 = (uint8_t)(g | b);
+        genBound8 = (uint8_t)(g | special | b);
         depth8    = (int8_t)(d / ONE_PLY);
     }
   }
@@ -97,7 +99,7 @@ class TranspositionTable {
 
 public:
  ~TranspositionTable() { free(mem); }
-  void new_search() { generation8 += 4; } // Lower 2 bits are used by Bound
+  void new_search() { generation8 += 8; } // Lower 3 bits are used otherwise
   uint8_t generation() const { return generation8; }
   TTEntry* probe(const Key key, bool& found) const;
   int hashfull() const;
