548d8498a611206d9d87c59e5fa9db32b4f2a95c
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-03-20 13:41:56 +0100

    Refine definition of advanced pawn push
    bench: 6039657

--- src/position.h
+++ src/position.h
@@ -313,7 +313,8 @@ inline bool Position::pawn_passed(Color c, Square s) const {
 
 inline bool Position::advanced_pawn_push(Move m) const {
   return   type_of(moved_piece(m)) == PAWN
-        && relative_rank(sideToMove, from_sq(m)) > RANK_4;
+        && relative_rank(sideToMove, from_sq(m)) > RANK_4
+		&& (!non_pawn_material(~sideToMove) || pawn_passed(sideToMove, to_sq(m)) || (StepAttacksBB[KING][from_sq(m)] & pieces(sideToMove, PAWN)));
 }
 
 inline Key Position::key() const {
