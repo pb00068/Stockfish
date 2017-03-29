5ff21f959914b48ffd3937266fbd91353978c89c
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-03-29 09:58:55 +0200

    No internal iterative deepening on all nodes
    The chance of a predicted all-node to become a cut-node or PV-node is
    low, so probably IID will not a best move at all.
    
    bench: 5992557

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
     // Step 10. Internal iterative deepening (skipped when in check)
     if (    depth >= 6 * ONE_PLY
         && !ttMove
-        && (PvNode || ss->staticEval + 256 >= beta))
+        && (PvNode || (cutNode && ss->staticEval + 256 >= beta)))
     {
         Depth d = (3 * depth / (4 * ONE_PLY) - 2) * ONE_PLY;
         search<NT>(pos, ss, alpha, beta, d, cutNode, true);
