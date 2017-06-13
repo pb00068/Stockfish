df9340fa5564fe3193813fb7b2f6609327546cc1
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-06-13 10:13:23 +0200

    PVS and Aspiration: immediately research wider if first move fails low
    bench: 5709274

--- src/search.cpp
+++ src/search.cpp
@@ -1065,8 +1065,11 @@ moves_loop: // When in check search starts from here
               // the best move changes frequently, we allocate some more time.
               if (moveCount > 1 && thisThread == Threads.main())
                   ++static_cast<MainThread*>(thisThread)->bestMoveChanges;
+              if (value <= alpha && beta - alpha <  Value(40)) // fail-low on first move on a small aspiration window
+                 return value;
           }
           else
+
               // All other moves but the PV are set to the lowest value: this is
               // not a problem when sorting because the sort is stable and the
               // move position in the list is preserved - just the PV is pushed up.
