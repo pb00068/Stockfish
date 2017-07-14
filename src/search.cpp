69aab7ed539fbf9b51fc9660ca704dbf40fe3597
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-07-14 07:56:24 +0200

    bench: 6270513

--- src/search.cpp
+++ src/search.cpp
@@ -920,11 +920,10 @@ moves_loop: // When in check search starts from here
 
               // Prune moves with negative SEE
               if (   lmrDepth < 8
-                  && !excludedMove
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (    depth < 7 * ONE_PLY
+          else if (    depth < 8 * ONE_PLY
                    && !extension
                    && !excludedMove
                    && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
