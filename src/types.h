d8d4e554dbae55af214f93d106e65805a8c05c79
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-10-16 14:00:27 +0200

    Extended, 2 level, knight mobility
    bench: 5314323

--- src/types.h
+++ src/types.h
@@ -184,7 +184,7 @@ enum Value : int {
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE + 2 * MAX_PLY,
 
   PawnValueMg   = 171,   PawnValueEg   = 240,
-  KnightValueMg = 758,   KnightValueEg = 848,
+  KnightValueMg = 764,   KnightValueEg = 848,
   BishopValueMg = 826,   BishopValueEg = 891,
   RookValueMg   = 1282,  RookValueEg   = 1373,
   QueenValueMg  = 2526,  QueenValueEg  = 2646,
