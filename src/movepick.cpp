3dbeb10ae40d8f154e5a1b1f9e7551203bf820c1
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-11-09 13:57:38 +0100

    bench: 5240694

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -134,11 +134,13 @@ void MovePicker::score() {
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
                    + Value((*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]);
 
-      else if (Type == QUIETS)
+      else if (Type == QUIETS) {
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + (*contHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*contHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   + (*contHistory[3])[pos.moved_piece(m)][to_sq(m)];
+                   + (*contHistory[3])[pos.moved_piece(m)][to_sq(m)]
+			       + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]; // N.B.: underpromotions are 'quiets'
+      }
 
       else // Type == EVASIONS
       {
