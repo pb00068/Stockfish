400a9f6adedae5ae7dcbef1b3a6e8119be5aa9bc
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-11-24 12:01:31 +0100

    Lmr-prune more towards the leaves
    bench: 4404858

--- src/search.cpp
+++ src/search.cpp
@@ -956,6 +956,9 @@ moves_loop: // When in check search starts from here
               else if ((ss-1)->statScore >= 0 && ss->statScore < 0)
                   r += ONE_PLY;
 
+              if (ss->ply > thisThread->rootDepth - 1)
+            	  r += ONE_PLY;
+
               // Decrease/increase reduction for moves with a good/bad history
               r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->statScore / 20000) * ONE_PLY);
           }
