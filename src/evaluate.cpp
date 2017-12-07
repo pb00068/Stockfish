e0a93218fea5d0f57ea21ea159fa27d878bbc6f1
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-12-07 17:49:30 +0100

    Take 2
    bench: 5939055

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -230,7 +230,7 @@ namespace {
   const Score ThreatByAttackOnQueen = S( 38, 22);
   const Score HinderPassedPawn      = S(  7,  0);
   const Score TrappedBishopA1H1     = S( 50, 50);
-  const Score KingQueenLineAttack   = S( 10, 16);
+  const Score KingQueenLineAttack   = S( 18, 36);
 
   #undef S
   #undef V
