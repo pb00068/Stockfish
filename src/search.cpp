19167f90c2e8347d31449ace908e589fd17a2d9b
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-01-10 08:41:35 +0100

    further simplification, non-functional change
    verified same bench up to depth 18
    bench: 5221753

--- src/search.cpp
+++ src/search.cpp
@@ -676,7 +676,7 @@ namespace {
     if (   !PvNode
         &&  eval >= beta
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
-        && (ss->ply >= thisThread->nmp_ply || ss->ply % 2 == thisThread->pair))
+        && (ss->ply >= thisThread->nmp_ply || ss->ply % 2 != thisThread->nmp_odd))
     {
 
         assert(eval - beta >= 0);
@@ -705,12 +705,11 @@ namespace {
             R +=  (depth / ONE_PLY - 7) * ONE_PLY / 5;
             // disable null move pruning for side to move for the first part of the remaining search tree
             thisThread->nmp_ply = ss->ply + 3 * (depth-R) / 4;
-            thisThread->pair = (ss->ply % 2) == 0;
+            thisThread->nmp_odd = ss->ply % 2;
 
             Value v = depth-R < ONE_PLY ? qsearch<NonPV, false>(pos, ss, beta-1, beta)
                                         :  search<NonPV>(pos, ss, beta-1, beta, depth-R, false, true);
-            thisThread->pair = -1;
-            thisThread->nmp_ply = 0;
+            thisThread->nmp_odd = thisThread->nmp_ply = 0;
 
             if (v >= beta)
                 return nullValue;
@@ -876,7 +875,7 @@ moves_loop: // When in check search starts from here
                   continue;
 
               // Prune moves with negative SEE
-              if (    lmrDepth < 8
+              if (   lmrDepth < 8
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
           }
