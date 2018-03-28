8e4036ddde33574c84fbc7a0172501eca3f4c1b6
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-03-28 14:18:00 +0200

    lower threshold when move is a discovering check
    bench: 5598208

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(move, Value(-55 * (cur-1)->value / 1024) - bool(pos.blockers_for_king(~pos.side_to_move()) & from_sq(move)) * 800) ?
+                       return pos.see_ge(move, Value(-55 * (cur-1)->value / 1024 - bool(pos.blockers_for_king(~pos.side_to_move()) & from_sq(move)) * 800)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = move, false); }))
           return move;
