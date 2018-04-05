87db1cfd26c2a662d59e943f7e6dcf1c73cd3af9
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-04-05 10:11:48 +0200

    Take 16
    bench: 5074045

--- src/search.cpp
+++ src/search.cpp
@@ -602,10 +602,8 @@ namespace {
             {
                 if (!pos.capture_or_promotion(ttMove))
                     update_quiet_stats(pos, ss, ttMove, nullptr, 0, stat_bonus(depth));
-                else if (to_sq(ttMove) != to_sq((ss-1)->currentMove) && type_of(pos.piece_on(to_sq(ttMove))) >= BISHOP && pos.see_ge(ttMove, BishopValueMg)) // && is_ok((ss+1)->currentMove) && !pos.capture((ss+1)->currentMove))
-                {
+                else if (to_sq(ttMove) != to_sq((ss-1)->currentMove) && type_of(pos.piece_on(to_sq(ttMove))) >= ROOK && pos.see_ge(ttMove, RookValueMg)) // && is_ok((ss+1)->currentMove) && !pos.capture((ss+1)->currentMove))
                 	 ss->weakSq = to_sq(ttMove);
-                }
 
                 // Extra penalty for a quiet TT move in previous ply when it gets refuted
                 if ((ss-1)->moveCount == 1 && !pos.captured_piece())
@@ -930,11 +928,12 @@ moves_loop: // When in check, search starts from here
                   && !pos.see_ge(move, Value(-35 * lmrDepth * lmrDepth)))
                   continue;
 
-              if (lmrDepth < 6 && (ss+1)->weakSq != SQ_NONE && type_of(movedPiece) < type_of(pos.piece_on((ss+1)->weakSq)))
+              if (lmrDepth < 6 && (ss+1)->weakSq != SQ_NONE && type_of(movedPiece) < type_of(pos.piece_on((ss+1)->weakSq))
+            		  && !pos.see_ge(make_move((ss+1)->weakSq,(ss+1)->weakSq), PieceValue[MG][type_of(pos.piece_on((ss+1)->weakSq))] ))
               {
             	  // don't prune if our move is going to defend our weak Square or attacking other heavy pieces
             	  bool veto = (type_of(movedPiece) == PAWN ? pos.attacks_from<PAWN>(to_sq(move), pos.side_to_move()) : pos.attacks_from(type_of(movedPiece), to_sq(move) ) &
-            			  ((ss+1)->weakSq | (pos.pieces(~pos.side_to_move()) ^ pos.pieces(~pos.side_to_move(), PAWN))));
+            			  ((pos.pieces(~pos.side_to_move()) ^ pos.pieces(~pos.side_to_move(), PAWN)) | (ss+1)->weakSq ));
             	  if (!veto)
 				   continue;
               }
@@ -1140,7 +1139,7 @@ moves_loop: // When in check, search starts from here
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
         else {
             update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth));
-            if (to_sq(bestMove) != to_sq((ss-1)->currentMove) && type_of(pos.piece_on(to_sq(bestMove))) >= BISHOP && pos.see_ge(bestMove, BishopValueMg)) // && is_ok((ss+1)->currentMove) && !pos.capture((ss+1)->currentMove))
+            if (to_sq(bestMove) != to_sq((ss-1)->currentMove) && type_of(pos.piece_on(to_sq(bestMove))) >= ROOK && pos.see_ge(bestMove, RookValueMg)) // && is_ok((ss+1)->currentMove) && !pos.capture((ss+1)->currentMove))
             {
             	 ss->weakSq = to_sq(bestMove);
             	 //sync_cout << pos << UCI::move(bestMove, pos.is_chess960()) << " follow: " <<  UCI::move((ss+1)->currentMove, pos.is_chess960()) << sync_endl;
