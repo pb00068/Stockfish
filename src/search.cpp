5805c06aeca14c4ab3599b18ef25d9cea505a5c3
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-04-18 12:23:06 +0200

    Trigger skipquiets only if not in check or moved piece is not king
    bench: 5643898

--- src/search.cpp
+++ src/search.cpp
@@ -899,7 +899,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
-              if (moveCountPruning)
+              if (moveCountPruning && (!inCheck || type_of(movedPiece) != KING))
               {
                   skipQuiets = true;
                   continue;
