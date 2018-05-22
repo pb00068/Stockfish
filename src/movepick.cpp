72140667766c9917d9c6de4c6a5038dc645b4356
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-05-22 09:41:47 +0200

    Take 2
    bench:  4586636

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                    return   ((pos.blockers_for_king(~pos.side_to_move()) & from_sq(move)) || pos.see_ge(move, Value(-55 * (cur-1)->value / 1024))) ?
+                    return   (pos.see_ge(move, Value(-55 * (cur-1)->value / 1024 - bool(pos.blockers_for_king(~pos.side_to_move()) & from_sq(move)) * KnightValueMg ))) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = move, false); }))
           return move;
