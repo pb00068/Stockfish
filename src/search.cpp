2a57a01c333ba96de3d28fc5db857ee818ae63da
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-06-12 15:16:48 +0200

    take 4: apply a more linear formula for capture statistics
    bench: 5046983

--- src/search.cpp
+++ src/search.cpp
@@ -823,15 +823,9 @@ namespace {
                 pos.undo_move(move);
 
                 if (value >= rbeta)
-                {
-                	update_capture_stats(pos, move, capturesSearched, captureCount, capture_stat_bonus(depth));
                     return value;
-                }
-                capturesSearched[captureCount++] = move;
             }
     }
-
-    captureCount = 0;
 
     // Step 11. Internal iterative deepening (~2 Elo)
     if (    depth >= 8 * ONE_PLY
