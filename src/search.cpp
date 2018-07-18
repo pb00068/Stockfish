bf59f371000ac4c27ec9ecb52b1e9d375bc773b7
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-07-18 12:30:55 +0200

    Take 4
    bench:  4432652

--- src/search.cpp
+++ src/search.cpp
@@ -1164,8 +1164,8 @@ moves_loop: // When in check, search starts from here
         	if (!pos.captured_piece())
         		update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + ONE_PLY));
         	else if (prevSq != to_sq(bestMove)){
-                PieceType captured = type_of(pos.captured_piece());
-                pos.this_thread()->captureHistory[pos.piece_on(prevSq)][prevSq][captured] << -stat_bonus(depth + ONE_PLY);
+                if (pos.this_thread()->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] > 0)
+                	pos.this_thread()->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] << -stat_bonus(depth + ONE_PLY);
         	}
         }
     }
