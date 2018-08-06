bc1f5ee9947177cb86525d699bc72aee33615513
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-08-06 09:55:39 +0200

    Track 3rd killer but use it only when first or second is ttMove
    or a capture
    
    bench: 4617892

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -62,7 +62,7 @@ namespace {
 MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHistory* mh,
                        const CapturePieceToHistory* cph, const PieceToHistory** ch, Move cm, Move* killers)
            : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch),
-             refutations{{killers[0], 0}, {killers[1], 0}, {killers[2], 0}, {cm, 0}}, depth(d) {
+             refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}, {killers[2], 0}}, depth(d) {
 
   assert(d > DEPTH_ZERO);
 
@@ -183,14 +183,16 @@ top:
 
       // Prepare the pointers to loop over the refutations array
       cur = std::begin(refutations);
-      endMoves = std::end(refutations);
+      endMoves = std::end(refutations) - 1;
 
-      if (refutations[0].move == ttMove || pos.capture(refutations[0].move))
-    	  refutations[0].move = refutations[2].move;
+      if ((refutations[0].move == ttMove || pos.capture(refutations[0].move)) && refutations[1].move != refutations[3].move)
+    	  refutations[0].move = refutations[3].move;
+      else if ((refutations[1].move == ttMove || pos.capture(refutations[1].move)) && refutations[0].move != refutations[3].move)
+    	  refutations[1].move = refutations[3].move;
 
       // If the countermove is the same as a killer, skip it
-      if (   refutations[0].move == refutations[3].move
-          || refutations[1].move == refutations[3].move)
+      if (   refutations[0].move == refutations[2].move
+          || refutations[1].move == refutations[2].move)
           --endMoves;
 
       ++stage;
