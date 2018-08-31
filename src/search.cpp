60625fbc1a8338433001b48b1a9426cdfa26f951
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-08-31 08:52:54 +0200

    Extend single non-capture-evasion moves if tree is'nt yet ext., take 2
    bench: 4481511

--- src/search.cpp
+++ src/search.cpp
@@ -888,9 +888,10 @@ moves_loop: // When in check, search starts from here
       captureOrPromotion = pos.capture_or_promotion(move);
       // extend single evasion moves if subtree is'nt extended already
       extension = inCheck
-       && !excludedMove
        && !moveCount
        && !ttMove
+       && !excludedMove
+	   && (ss-1)->moveCount < 6
        && depth <= thisThread->rootDepth - ss->ply
        && !mp.hasNextMoveInStage()
        && !captureOrPromotion
