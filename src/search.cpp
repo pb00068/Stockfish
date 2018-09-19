b9374fda078ecebed4a027889aa241ce8b0ac1ba
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-09-19 12:10:52 +0200

    Take 5
    bench: 4857968

--- src/search.cpp
+++ src/search.cpp
@@ -442,19 +442,18 @@ void Thread::search() {
                      StateInfo st1, st2;
                      rootPos.do_move(rootMoves[0].pv[0], st1);
                      rootPos.do_move(rootMoves[0].pv[1], st2);
-	                 sync_cout << "info researching with depth: " << Depth(selDepth) << " bestv: " << bestValue << " a: " << alpha << " b: " << beta << " failLow =" << failedLow << sync_endl;
                      Move pv[MAX_PLY+1];
 
                      (ss+2)->pv = pv;
                      (ss+2)->pv[0] = MOVE_NONE;
-                     Depth d = std::min(std::max(Depth(selDepth), rootDepth - 2 * ONE_PLY), rootDepth);
+                     Depth d = std::min(std::max(Depth(selDepth), rootDepth - 4 * ONE_PLY), rootDepth - ONE_PLY);
+                     //sync_cout << "info selDepth " << selDepth << " researching with depth: " << d << " bestv: " << bestValue << " a: " << alpha << " b: " << beta << " rootd: " << rootDepth<< sync_endl;
                      selDepth = 0;
                      bestValue = ::search<PV>(rootPos, (ss+2), alpha, beta, d, false);
 
                      rootPos.undo_move(rootMoves[0].pv[1]);
                      rootPos.undo_move(rootMoves[0].pv[0]);
-                     sync_cout << "info have done a research bestv: " << bestValue << " seld: " << selDepth << sync_endl;
-
+                     //sync_cout << "info have done a research bestv: " << bestValue << " seld: " << selDepth << sync_endl;
                      if (bestValue >= beta)
                      {
                         Value b = std::min(bestValue + delta, VALUE_INFINITE);
