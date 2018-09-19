57d187d8495835c7fac934d01b94722f20322200
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-09-19 09:35:50 +0200

    Take 3
    bench:  4720059

--- src/search.cpp
+++ src/search.cpp
@@ -439,7 +439,7 @@ void Thread::search() {
                   break;
 
               // do a fast verification search if selDepth is very low
-              if (selDepth < rootDepth + 3 && rootMoves[0].pv.size() >= 2)
+              if (selDepth < rootDepth && rootMoves[0].pv.size() >= 2)
               {
                   StateInfo st1, st2;
                   rootPos.do_move(rootMoves[0].pv[0], st1);
@@ -452,18 +452,19 @@ void Thread::search() {
                  Depth d = Depth(selDepth);
                  selDepth = 0;
                  bestValue = ::search<PV>(rootPos, (ss+2), alpha, beta, d , false);
-                 //sync_cout << "info have done a research bestv: " << bestValue << " seld: " << selDepth << sync_endl;
-
-				 if (bestValue <= alpha)
-				 {
-					 beta = (alpha + beta) / 2;
-					 alpha = std::max(bestValue - delta, -VALUE_INFINITE);
-				 }
-                 else if (bestValue >= beta)
-	               beta = std::min(bestValue + delta, VALUE_INFINITE);
 
                  rootPos.undo_move(rootMoves[0].pv[1]);
                  rootPos.undo_move(rootMoves[0].pv[0]);
+                 //sync_cout << "info have done a research bestv: " << bestValue << " seld: " << selDepth << sync_endl;
+
+                 if (bestValue <= alpha)
+                 {
+                     beta = (alpha + beta) / 2;
+                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
+                 }
+                 else if (bestValue >= beta)
+	                 beta = std::min(bestValue + delta, VALUE_INFINITE);
+
               }
 
               delta += delta / 4 + 5;
