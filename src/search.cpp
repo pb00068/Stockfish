221e3eb11142b6071465ec9de9f0f8b6bcb57bbb
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-10-12 09:46:46 +0200

    Don't let king evasion moves influence move stats to much
    bench: 4534872

--- src/search.cpp
+++ src/search.cpp
@@ -1490,6 +1490,10 @@ moves_loop: // When in check, search starts from here
 
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
+
+    if (pos.checkers() && type_of(pos.moved_piece(move)) == KING)
+    	bonus/=8;
+
     thisThread->mainHistory[us][from_to(move)] << bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
