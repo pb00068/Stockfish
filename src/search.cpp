b8e8f43d9d4a4d99713d32c553cd97a6681f45cd
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-10-12 08:52:08 +0200

    move count prune stable all nodes a bit more
    bench: 4317709

--- src/search.cpp
+++ src/search.cpp
@@ -898,7 +898,7 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                        && moveCount + bool(depth > 5 * ONE_PLY && ttHit && !ttMove) >= FutilityMoveCounts[improving][depth / ONE_PLY];
 
       // Step 13. Extensions (~70 Elo)
 
