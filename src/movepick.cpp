481b0247c9c91eaf62c84e06504f648355f1a20a
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2018-10-18 10:15:07 +0200

    Discovering check capture always good
    bench: 4416761

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -176,6 +176,8 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
+    	               if (pos.blockers_for_king(~pos.side_to_move()) & from_sq(move))
+    	            	   return true;
                        return pos.see_ge(move, Value(-55 * (cur-1)->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = move, false); }))
