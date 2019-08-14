e589b32e095085d8eaecb67fc588a744e80aa96e
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-08-14 14:08:09 +0200

    More aggressive move count pruning after several good captures, take 4
    bench:  4253470

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount + (cutNode ? goodCaptureCount / 2 : 0) >= futility_move_count(improving, depth / ONE_PLY);
+          moveCountPruning = moveCount + (cutNode ?  0 : goodCaptureCount / 3) >= futility_move_count(improving, depth / ONE_PLY);
 
           if (   !captureOrPromotion
               && !givesCheck
