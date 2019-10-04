d3e267038fc3711ee058134548618f8ac97a0c99
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-10-04 08:32:01 +0200

    Refine save bitboard calculation
    bench: 4102443

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -553,7 +553,7 @@ namespace {
     if (pos.count<QUEEN>(Them) == 1)
     {
         Square s = pos.square<QUEEN>(Them);
-        safe = mobilityArea[Us] & ~stronglyProtected;
+        safe = mobilityArea[Us] & ~stronglyProtected & ~attackedBy[Them][KING];
 
         b = attackedBy[Us][KNIGHT] & pos.attacks_from<KNIGHT>(s);
 
