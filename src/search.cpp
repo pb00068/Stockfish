6893ab1c4eb342f12f53442b3117609506b54c8c
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-11-08 13:36:04 +0100

    Experiment (empty queue)
    bench:  4648878

--- src/search.cpp
+++ src/search.cpp
@@ -857,7 +857,7 @@ namespace {
         &&  depth >= 5
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value raisedBeta = std::min(beta + 191 - 46 * improving, VALUE_INFINITE);
+        Value raisedBeta = std::min(beta + 201 - 2 * depth - 46 * improving, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, raisedBeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 
