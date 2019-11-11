08a849bd5246118e7dae11c7d4172383682fdd26
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-11-11 09:33:46 +0100

    Probcut: verification search with depth 1 instad of 0 when negative SEE
    bench:  4142081

--- src/search.cpp
+++ src/search.cpp
@@ -877,10 +877,12 @@ namespace {
                                                                           [pos.moved_piece(move)]
                                                                           [to_sq(move)];
 
+                bool negSee = raisedBeta - ss->staticEval < 0 && !pos.see_ge(move, Value(0));
+
                 pos.do_move(move, st);
 
-                // Perform a preliminary qsearch to verify that the move holds
-                value = -qsearch<NonPV>(pos, ss+1, -raisedBeta, -raisedBeta+1);
+                // Perform a preliminary swallow search to verify that the move holds
+                value = -search<NonPV>(pos, ss+1, -raisedBeta, -raisedBeta+1, negSee * 1, !cutNode);
 
                 // If the qsearch held, perform the regular search
                 if (value >= raisedBeta)
