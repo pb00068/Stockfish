2cdc9865f6e410ee5b5cb220e4d94738f7ab29f0
pb00067 <pb00067@PHXL0356.wp.lan>
2019-11-21 12:23:16 +0100

    Exclude squares behind opposer pawn
    as enemy pawns can't push through our pawns
    Increases outposts from 629901 to 798188 (+26%) in a normal bench run.
    
    bench: 4912107

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -116,7 +116,7 @@ namespace {
         {
 
             e->pawnAttacksSpan[Us] |= pawn_attack_span(Us, s) ^ pawn_attacks_bb<Us>(opposed);
-            // probably we can't attack squares behind opposer
+            // probably we can't attack squares behind our opposer
             // N.B.: e->pawnAttacksSpan is only used for outposts so merely OutpostRanks (4,5,6) do matter
         }
 
