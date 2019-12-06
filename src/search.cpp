ecda353e891ba51196437cb329da7fa2b7b35658
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-12-06 15:41:03 +0100

    Empty framework experiment, take 1:
    bench: 5302609

--- src/search.cpp
+++ src/search.cpp
@@ -1117,13 +1117,13 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
-              // Increase reduction if ttMove is a capture (~0 Elo)
-              if (ttCapture)
-                  r++;
+
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
                   r += 2;
+              else if (ttCapture) // Increase reduction if ttMove is a capture (~0 Elo)
+                  r++;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
