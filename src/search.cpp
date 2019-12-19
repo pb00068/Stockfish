f2ce1ecd319599deb7de72890ec3a14468abd283
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2019-12-19 08:49:27 +0100

    Take 2
    bench:   5414169

--- src/search.cpp
+++ src/search.cpp
@@ -986,7 +986,7 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth, cutNode);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth, !cutNode);
 
           if (   !captureOrPromotion
               && !givesCheck)
