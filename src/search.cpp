e7fd7633333acd7b51acaf53501df32b91a470c0
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2020-02-04 09:44:01 +0100

    Experiment
    bench: 5094701

--- src/search.cpp
+++ src/search.cpp
@@ -1655,7 +1655,7 @@ moves_loop: // When in check, search starts from here
 
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus) {
 
-    if (ss->killers[0] != move)
+    if (ss->killers[0] != move && ((ss-1)->currentMove == MOVE_NULL || (ss-1)->moveCount > 2))
     {
         ss->killers[1] = ss->killers[0];
         ss->killers[0] = move;
