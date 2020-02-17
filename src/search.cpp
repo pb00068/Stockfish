c0b75d4c9e419d196ea60624d11883b804a684e3
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2020-02-17 08:09:34 +0100

    Take 6: same as Take 3 (278k LTC yellow), but reset
    (ss+2)->badGoodCapture instead to (ss+4)
    bench:  5115001

--- src/search.cpp
+++ src/search.cpp
@@ -672,7 +672,7 @@ namespace {
 
     (ss+1)->ply = ss->ply + 1;
     (ss+1)->excludedMove = bestMove = MOVE_NONE;
-    (ss+2)->killers[0] = (ss+2)->killers[1] = (ss+4)->badGoodCapture = MOVE_NONE;
+    (ss+2)->killers[0] = (ss+2)->killers[1] = (ss+2)->badGoodCapture = MOVE_NONE;
     Square prevSq = to_sq((ss-1)->currentMove);
 
     // Initialize statScore to zero for the grandchildren of the current position.
