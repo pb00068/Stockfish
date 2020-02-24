a32f5c15371c20721310b59dc83fee95ec119819
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2020-02-24 09:51:16 +0100

    Apply low-ply-history also on captures (bug-fix)
    bench: 5183013

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -110,7 +110,7 @@ void MovePicker::score() {
   for (auto& m : *this)
       if (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + (ply < MAX_LPH ?  4 * (*lowPlyHistory)[ply][from_to(m)] : 0)
+                   + (ply < MAX_LPH ? (*lowPlyHistory)[ply][from_to(m)] : 0)
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
