a1fd34ebabdc1fd9aff55faec6d724ea1c765cbc
pb00067 <pb00067@PHXL0356.wp.lan>
2020-03-18 15:28:40 +0100

    Take 1
    bench: 4962408

--- src/search.cpp
+++ src/search.cpp
@@ -1168,11 +1168,14 @@ moves_loop: // When in check, search starts from here
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ttPv;
 
-              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
-                             + (*contHist[0])[movedPiece][to_sq(move)]
+              ss->statScore =  (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
-                             + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 4926;
+                             + (*contHist[3])[movedPiece][to_sq(move)];
+
+              if (!ss->statScore)
+              	r--;
+
+              ss->statScore += thisThread->mainHistory[us][from_to(move)] - 4926;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -102 && (ss-1)->statScore < -114)
