8e1daea39f15e8ddf816bb2496d5d37e42b1f54f
pb00067 <pb00067@PHXL0356.wp.lan>
2020-04-22 08:21:09 +0200

    Less reduction coming out from a check in previous move
    Test gainst and on top of PR #2645
    bench:  4701966

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,9 @@ moves_loop: // When in check, search starts from here
           if (moveCountPruning && !formerPv)
               r++;
 
+          if (!ss->inCheck && (ss-2)->inCheck)
+          	r--;
+
           // Decrease reduction if opponent's move count is high (~5 Elo)
           if ((ss-1)->moveCount > 14)
               r--;
