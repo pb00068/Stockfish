1ac849ebb9645bec39d9b654f21798ccb984cbfd
pb00067 <pb00067@PHXL0356.wp.lan>
2020-04-24 09:18:07 +0200

    Take 1
    bench: 5005152

--- src/search.cpp
+++ src/search.cpp
@@ -1711,7 +1711,7 @@ moves_loop: // When in check, search starts from here
 
     for (int i : {1, 2, 4, 6})
     {
-        if (ss->inCheck && i > 2)
+        if ((ss->inCheck || (ss-2)->inCheck) && i > 2)
             break;
         if (is_ok((ss-i)->currentMove))
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
