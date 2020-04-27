6d45f307374487493b2e595dabd4f32d389420ba
pb00067 <pb00067@PHXL0356.wp.lan>
2020-04-27 11:40:25 +0200

    don't consider ss-6 cont history when holding evasion moves, variant 2
    bench: 4276208

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -109,7 +109,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
+                   + 2 * (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
                    + (ply < MAX_LPH ?  4 * (*lowPlyHistory)[ply][from_to(m)] : 0);
 
       else // Type == EVASIONS
