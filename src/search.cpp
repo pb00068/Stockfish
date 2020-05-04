9523d5692c3659389ffd701ca1cf6294593589df
pb00067 <pb00067@PHXL0356.wp.lan>
2020-05-04 14:19:46 +0200

    Take 1
    bench: 4490805

--- src/search.cpp
+++ src/search.cpp
@@ -158,7 +158,7 @@ namespace {
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus);
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, int depth);
   void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
-                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth);
+                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth, bool cutNode);
 
   // perft() is our utility to verify move generation. All the leaf nodes up
   // to the given depth are generated and counted, and the sum is returned.
@@ -1380,21 +1380,12 @@ moves_loop: // When in check, search starts from here
 
     else if (bestMove)
         update_all_stats(pos, ss, bestMove, bestValue, beta, prevSq,
-                         quietsSearched, quietCount, capturesSearched, captureCount, depth);
+                         quietsSearched, quietCount, capturesSearched, captureCount, depth, cutNode);
 
-
-    else {
-      // Bonus for prior countermove that caused the fail low
-      if (   (depth >= 3 || PvNode)
+    // Bonus for prior countermove that caused the fail low
+    else if (   (depth >= 3 || PvNode)
              && !priorCapture)
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
-
-      if (cutNode && quietCount && ttMove == quietsSearched[0])
-      {
-             thisThread->mainHistory[us][from_to(ttMove)] << -stat_bonus(depth - 1);
-             update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), -stat_bonus(depth - 1));
-      }
-    }
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
@@ -1678,7 +1669,7 @@ moves_loop: // When in check, search starts from here
   // update_all_stats() updates stats at the end of search() when a bestMove is found
 
   void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
-                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth) {
+                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth, bool cutNode) {
 
     int bonus1, bonus2;
     Color us = pos.side_to_move();
@@ -1688,7 +1679,7 @@ moves_loop: // When in check, search starts from here
     PieceType captured = type_of(pos.piece_on(to_sq(bestMove)));
 
     bonus1 = stat_bonus(depth + 1);
-    bonus2 = bestValue > beta + PawnValueMg ? bonus1               // larger bonus
+    bonus2 = bestValue > beta + PawnValueMg || !cutNode ? bonus1   // larger bonus
                                             : stat_bonus(depth);   // smaller bonus
 
     if (!pos.capture_or_promotion(bestMove))
