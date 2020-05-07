09faf6a1e7ef019a40848950982becdbe808d600
pb00067 <pb00067@PHXL0356.wp.lan>
2020-05-07 08:58:53 +0200

    exclude direct attacked pawns (not x-rayed)
    bench: 5033301

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -320,7 +320,7 @@ namespace {
                                      * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
 
                 // Penalty for all enemy pawns x-rayed
-                score -= BishopXRayPawns * popcount(PseudoAttacks[BISHOP][s] & pos.pieces(Them, PAWN));
+                score -= BishopXRayPawns * popcount(PseudoAttacks[BISHOP][s] & pos.pieces(Them, PAWN) & ~b);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
