0c6d30be7efff974fad5db13dab0f7ab02a410b4
pb00067 <pb00067@PHXL0356.wp.lan>
2020-05-11 09:58:39 +0200

    Take 1: less bonus/penalty in mainhistory when in check
    bench: 4483615

--- src/search.cpp
+++ src/search.cpp
@@ -1689,7 +1689,7 @@ moves_loop: // When in check, search starts from here
         // Decrease all the non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
         {
-            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
+            thisThread->mainHistory[us][from_to(quietsSearched[i])] << (ss->inCheck ? -bonus2/4 : -bonus2);
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
     }
@@ -1738,7 +1738,7 @@ moves_loop: // When in check, search starts from here
 
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
-    thisThread->mainHistory[us][from_to(move)] << bonus;
+    thisThread->mainHistory[us][from_to(move)] << (ss->inCheck ? bonus/4 : bonus);
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
     if (type_of(pos.moved_piece(move)) != PAWN)
