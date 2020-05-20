445b5121f959ebe3428037f072c28a086a96ee2c
pb00067 <pb00067@PHXL0356.wp.lan>
2020-05-20 09:38:17 +0200

    Take 1, test against and on top of PR#2688
    bench:  4673399

--- src/search.cpp
+++ src/search.cpp
@@ -974,7 +974,7 @@ moves_loop: // When in check, search starts from here
                                       contHist,
                                       countermove,
                                       ss->killers,
-                                      depth > 12 ? ss->ply : MAX_PLY);
+                                      depth > 6 ? ss->ply : MAX_PLY);
 
     value = bestValue;
     singularLMR = moveCountPruning = false;
