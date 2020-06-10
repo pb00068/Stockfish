e6269cea5e97956205da36b212d9f33620e7e892
pb00067 <pb00067@PHXL0356.wp.lan>
2020-06-10 13:36:59 +0200

    Take 6, fix
    bench: 4532442

--- src/search.cpp
+++ src/search.cpp
@@ -1714,7 +1714,7 @@ moves_loop: // When in check, search starts from here
     Thread* thisThread = pos.this_thread();
     Piece mp = pos.moved_piece(move);
     thisThread->mainHistory[us][from_to(move)] << bonus;
-    if (bonus > stat_bonus(depth + 1))
+    if (bonus > stat_bonus(depth))
         thisThread->gamePlyTrigger[mp][to_sq(move)] = pos.game_ply();
     update_continuation_histories(ss, mp, to_sq(move), bonus);
 
