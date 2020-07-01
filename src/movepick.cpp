1907f5145aff950390f08ed5a608c0e27ef9e232
pb00067 <pb00067@PHXL0356.wp.lan>
2020-07-01 09:00:45 +0200

    Capture killer take 4
    bench:  4510476

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -103,10 +103,11 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if (Type == CAPTURES)
+      {
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + (m.move == captkiller ? int(PieceValue[MG][ROOK]) : 0)
+                   + (m.move == captkiller ? int(QueenValueMg) : 0)
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
-
+      }
       else if (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
@@ -172,7 +173,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return  pos.see_ge(*cur, Value((cur->move == captkiller ? -80 : -69 )* cur->value / 1024)) ?
+                       return  pos.see_ge(*cur, Value((cur->move == captkiller ? -80 : -60 )* cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
