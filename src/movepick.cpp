c2462446080ba704bd34c573af1ef9aa763d2357
pb00067 <pb00067@PHXL0356.wp.lan>
2020-07-06 09:08:11 +0200

    'bad' capture bestmove: assign bonus until it get's a 'good' capture
    bench: 5019493

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -101,8 +101,13 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if (Type == CAPTURES)
-          m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+      {
+          m.value =  (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          if (m.value >= 10692)
+              m.value *=2;
+          else
+             m.value =+ int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6;
+      }
 
       else if (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
