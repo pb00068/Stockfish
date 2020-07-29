09c24600b198393b4dfa400f15ddeb17633c84b8
pb00067 <pb00067@PHXL0356.wp.lan>
2020-07-30 00:51:13 +0200

    Take 1: reduce QueenValueMg (on top of snicolets PR)
    bench: 4004882

--- src/types.h
+++ src/types.h
@@ -184,7 +184,7 @@ enum Value : int {
   KnightValueMg = 781,   KnightValueEg = 854,
   BishopValueMg = 825,   BishopValueEg = 915,
   RookValueMg   = 1276,  RookValueEg   = 1380,
-  QueenValueMg  = 2538,  QueenValueEg  = 2682,
+  QueenValueMg  = 2505,  QueenValueEg  = 2682,
   Tempo = 28,
 
   MidgameLimit  = 15258, EndgameLimit  = 3915
