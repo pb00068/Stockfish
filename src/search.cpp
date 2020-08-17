2868cd2119f47d5b10b9b2574643fa81bcccad25
pb00067 <pb00067@PHXL0356.wp.lan>
2020-08-17 12:17:43 +0200

    Don't penalize quiet move if it was a killer in sibling node
    bench: 4674908

--- src/search.cpp
+++ src/search.cpp
@@ -1364,7 +1364,7 @@ moves_loop: // When in check, search starts from here
           if (captureOrPromotion && captureCount < 32)
               capturesSearched[captureCount++] = move;
 
-          else if (!captureOrPromotion && quietCount < 64)
+          else if (!captureOrPromotion && quietCount < 64 && move != ss->killers[0] &&  move != ss->killers[1])
               quietsSearched[quietCount++] = move;
       }
     }
