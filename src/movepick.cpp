cfee7e1422a6bf0f6d723dcf87d53cf9e6755359
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2020-09-10 10:44:14 +0200

    draft

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -103,7 +103,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
       {
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + (m.move == captkiller ? int(QueenValueMg) : 0)
+                   //+ (m.move == captkiller ? int(QueenValueMg) : 0)
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
       }
       else if (Type == QUIETS)
