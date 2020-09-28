d7c4896b28d05bbd8d0837ddd127fa923c376516
pb00067 <pb00067@PHXL0356.wp.lan>
2020-09-28 10:01:49 +0200

    Take 1
    bench:   4181701

--- src/search.cpp
+++ src/search.cpp
@@ -1022,10 +1022,13 @@ moves_loop: // When in check, search starts from here
               && !givesCheck)
           {
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+              if (   lmrDepth < 5 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
-                  continue;
+              {
+                  if (lmrDepth < 4 || (*contHist[3])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                     continue;
+              }
 
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
