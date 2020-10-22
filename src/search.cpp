d51d4b934de0131a7821358cda989b6a30fa43b1
pb00067 <pb00067@PHXL0356.wp.lan>
2020-10-22 11:54:38 +0200

    Take 4
    bench:   4210899

--- src/search.cpp
+++ src/search.cpp
@@ -1183,8 +1183,6 @@ moves_loop: // When in check, search starts from here
               int f = thisThread->failedLowCnt + thisThread->failedHighCnt;
               r += rootNode ? f * f * moveCount / 512 : 0;
 
-              if (ss->ply == 1)
-                 r += thisThread->failedLowCnt * thisThread->failedLowCnt * moveCount / 256;
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
