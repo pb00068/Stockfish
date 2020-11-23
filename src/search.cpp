e274e5077d5794d7c833c7dc0a5d9a2340d76a51
pb00067 <pb00067@PHXL0356.wp.lan>
2020-11-23 16:18:57 +0100

    Take 2
    bench:  3934143

--- src/search.cpp
+++ src/search.cpp
@@ -1211,7 +1211,7 @@ moves_loop: // When in check, search starts from here
                              - 5287;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
-              if (ss->statScore >= -106 && (ss-1)->statScore < -104)
+              if (ss->statScore >= -106 && ((ss-1)->statScore < -104 || (ss-1)->statScore == 0))
                   r--;
 
               else if ((ss-1)->statScore >= -119 && ss->statScore < -140)
