33d0e0f5787a82067a9aee60b42797e8fed75c81
pb00067 <pb00067@PHXL0356.wp.lan>
2020-12-15 09:04:09 +0100

    Take 1 : compensate for now higher values in mainHistory
    bench: 4182234

--- src/search.cpp
+++ src/search.cpp
@@ -1228,7 +1228,8 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5287;
+                             - 5287 - 2243;
+
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -105 && (ss-1)->statScore < -103)
