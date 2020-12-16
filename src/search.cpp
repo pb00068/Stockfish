209056e3b1c3f28c10bcc4e082b3bf03ca29fa73
pb00067 <pb00067@PHXL0356.wp.lan>
2020-12-16 10:09:10 +0100

    Take 2, Write stat-bonus also on tt-cutoffs, test on top of passed test
    (pull 3265)
    
    bench:  3785867

--- src/search.cpp
+++ src/search.cpp
@@ -722,7 +722,12 @@ namespace {
         // For high rule50 counts don't produce transposition table cutoffs.
         if (pos.rule50_count() < 90)
         {
-            if (is_ok((ss-1)->currentMove) && !ss->inCheck && !(ss-1)->inCheck && !priorCapture && tte->eval() != VALUE_NONE)
+            if (ttValue >= beta
+                && (tte->bound() & BOUND_LOWER)
+                && is_ok((ss-1)->currentMove)
+                && !ss->inCheck
+                && !(ss-1)->inCheck
+                && !priorCapture)
             {
                 int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + tte->eval() - 2 * Tempo), -1000, 1000);
                 thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
