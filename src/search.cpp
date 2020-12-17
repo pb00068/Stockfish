129aa6cfe9aa95e64ad266800ebe86f0f9209262
pb00067 <pb00067@PHXL0356.wp.lan>
2020-12-17 10:31:31 +0100

    Take 2
    bench: 4023382

--- src/search.cpp
+++ src/search.cpp
@@ -816,7 +816,7 @@ namespace {
     }
 
     // Use static evaluation difference to improve quiet move ordering
-    if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture && abs((ss-1)->staticEval + ss->staticEval) > 60) {
+    if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture && abs((ss-1)->staticEval + ss->staticEval) > 30) {
         int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval - 2 * Tempo), -1000, 1000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
     }
