6574a34229471168107a88714c2f28ca1012ee1e
pb00067 <pb00067@PHXL0356.wp.lan>
2020-12-21 09:09:21 +0100

    Take 5: don't shallow prune moves with negative SEE where
    the SEE exchanges leaves the enemy king/queen under attack
    bench: 4134963

--- src/search.cpp
+++ src/search.cpp
@@ -1069,7 +1069,7 @@ moves_loop: // When in check, search starts from here
               // Prune moves with negative SEE (~20 Elo)
               if (!pos.see_ge(move, occupied_after_see, Value(-(30 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
               {
-                  if (lmrDepth < 4 || !(pos.slider_attackers_to(pos.pieces(~us, KING, QUEEN) | pos.pieces(~us, ROOK), occupied_after_see) & pos.pieces(us) & occupied_after_see))
+                  if (type_of(movedPiece) >= ROOK || !(pos.slider_attackers_to(pos.pieces(~us, KING, QUEEN), occupied_after_see) & pos.pieces(us) & occupied_after_see))
                     continue;
               }
           }
@@ -1085,7 +1085,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, occupied_after_see, Value(-213) * depth)) // (~25 Elo)
               {
                   occupied_after_see = occupied_after_see | to_sq(move);
-                  if (depth < 4 || !(pos.slider_attackers_to(pos.pieces(~us, KING, QUEEN) | pos.pieces(~us, ROOK), occupied_after_see) & pos.pieces(us) & occupied_after_see))
+                  if (type_of(movedPiece) >= ROOK || !(pos.slider_attackers_to(pos.pieces(~us, KING, QUEEN), occupied_after_see) & pos.pieces(us) & occupied_after_see))
                      continue;
               }
           }
