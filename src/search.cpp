282bfa7c67e3bf1935ccd8d3bbd24f58f26826be
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-04 11:15:07 +0100

    Take 3
    bench:  4038452

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 1 + bool(ttMove == MOVE_NONE);
+                  r += 2 * !bool(ss->ttPv);
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
