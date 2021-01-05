3a9b4b0cd7712bd8174ef2329ce825a0dddaaf4a
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-05 09:28:56 +0100

    Take 3
    bench: 4375604

--- src/search.cpp
+++ src/search.cpp
@@ -840,8 +840,8 @@ namespace {
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22977
-        &&  eval >= beta - 8
-        &&  eval >= ss->staticEval - 8
+        &&  eval >= beta - 2
+        &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 30 * depth - 28 * improving + 84 * ss->ttPv + 168
         && !excludedMove
         &&  pos.non_pawn_material(us)
