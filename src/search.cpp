f68bfe7f44ce96efa08316b9e5f43394efb2c405
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-05 15:59:20 +0100

    Don't set staticEval to VALUE_NONE when in check
    bench: 4109337

--- src/search.cpp
+++ src/search.cpp
@@ -1231,7 +1231,8 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Unless giving check, this capture is likely bad
-              if (   !givesCheck && !ss->inCheck
+              if (   !givesCheck
+                  && !ss->inCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
           }
