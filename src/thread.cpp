c1b20554801b86ca40d03fff0f627b5b27849d6b
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-13 18:28:04 +0100

    I don't see any purpose(use) in initialazing this with an extra value -1
    bench: 4382409

--- src/thread.cpp
+++ src/thread.cpp
@@ -66,7 +66,6 @@ void Thread::clear() {
           for (auto& to : continuationHistory[inCheck][c])
                 for (auto& h : to)
                       h->fill(0);
-          continuationHistory[inCheck][c][NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
       }
 }
 
