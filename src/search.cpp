397065f6762b2f6f37a6be7039c519d7ad7d82b5
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-14 14:15:01 +0100

    Take 3
    bench:  3818502

--- src/search.cpp
+++ src/search.cpp
@@ -1200,9 +1200,8 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
-
-              if (!givesCheck && cutNode && bestGain > KnightValueMg - PawnValueMg && !pos.see_ge(move, bestGain))
-                  r ++;
+              else if (!givesCheck && bestGain > KnightValueMg && !pos.see_ge(move, bestGain - BishopValueMg))
+                  r++;
           }
           else
           {
