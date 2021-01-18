c58e1a3d0508867d3dd9e8217d8b4591bd6dd148
pb00067 <pb00067@PHXL0356.wp.lan>
2021-01-18 10:23:20 +0100

    Revert "Take 2: movepick: use follow move histories only when improving"
    This reverts commit 6bcdebab90490f5f437600cffc3f8710031836be.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -106,8 +106,8 @@ void MovePicker::score() {
       else if (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                   +  (improving ? (  2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
+                   + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
+                   +  (improving ? (2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]) : 0)
                    + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
 
