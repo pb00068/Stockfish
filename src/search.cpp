9f59adfb237c5a5667274e2085b604872458ef77
pb00067 <pb00067@PHXL0356.wp.lan>
2021-03-03 15:28:27 +0100

    Take 2 fixed (thanks to Viz) also this with extreme low bench
    bench: 2830763

--- src/search.cpp
+++ src/search.cpp
@@ -1281,7 +1281,7 @@ moves_loop: // When in check, search starts from here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
-          if (value <= alpha && !captureOrPromotion)
+          if (value <= alpha && !captureOrPromotion && d > 1)
               update_continuation_histories(ss, movedPiece, to_sq(move), -stat_bonus(d-1));
       }
       else
