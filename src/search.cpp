dd1a3abfcad9bfe64a340eaab67b1a032371b3ab
pb00067 <pb00067@PHXL0356.wp.lan>
2021-03-03 18:06:36 +0100

    Take 1: allow more than nominal depth also on first non-pv moves
    near the PV
    bench: 5433499

--- src/search.cpp
+++ src/search.cpp
@@ -1282,7 +1282,7 @@ moves_loop: // When in check, search starts from here
 
           if (moveCount != 1)
           {
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, !cutNode || moveCount > 1);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
