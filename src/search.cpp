7962df7c6a6f5d779bf82db4dd4a873edac69e12
pb00067 <pb00067@PHXL0356.wp.lan>
2021-05-10 17:48:28 +0200

    Take 3 (= logical fix of take 2)
    bench: 3711611

--- src/search.cpp
+++ src/search.cpp
@@ -654,7 +654,7 @@ namespace {
     (ss+1)->ttPv = false;
     (ss+1)->excludedMove = bestMove = MOVE_NONE;
     (ss+2)->killers[0] = (ss+2)->killers[1] = MOVE_NONE;
-    (ss+2)->nmpruned[0] = (ss+2)->nmpruned[1] = MOVE_NONE;
+    (ss+2)->nmpruned[0] = (ss+2)->nmpruned[1] = (ss+2)->nmpruned[2] = MOVE_NONE;
     Square prevSq = to_sq((ss-1)->currentMove);
 
     // Initialize statScore to zero for the grandchildren of the current position.
