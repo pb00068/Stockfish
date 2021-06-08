7c490e323e199e07b2c844c724be566662d0dd7d
pb00067 <pb00067@PHXL0356.wp.lan>
2021-06-08 09:31:44 +0200

    Take 2
    bench: 4902921

--- src/search.cpp
+++ src/search.cpp
@@ -1164,10 +1164,10 @@ moves_loop: // When in check, search starts from here
               r += 1 + !captureOrPromotion;
           // Decrease reduction for moves that escape a capture. Filter out
           // castling moves, because they are coded as "king captures rook" and
-          // hence break reverse_move() (~2 Elo)
-          else if (    !ttMove && type_of(move) == NORMAL
+          // hence break reverse_move()
+          else if (    newDepth - r < 4 && type_of(move) == NORMAL
                       && !pos.see_ge(reverse_move(move)))
-              r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
+              r -= 2;
 
           if (!captureOrPromotion)
           {
