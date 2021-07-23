69ae9fbaf45049105943a03d5bb47e4435024c75
pb00067 <pb00067@PHXL0356.wp.lan>
2021-07-23 13:26:05 +0200

    Take 6: take 3 + adjustment of stat_bonus for depths > 14
    bench: 5089956

--- src/search.cpp
+++ src/search.cpp
@@ -83,7 +83,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return d > 14 ? 73 : 6 * d * d + 229 * d - 215;
+    return d > 14 ? 4400 : 6 * d * d + 229 * d - 215;
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
