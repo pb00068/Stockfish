dff5f83d54dc90bed2f8e62a225dced26e535404
pb00067 <pb00067@PHXL0356.wp.lan>
2021-08-31 15:07:53 +0200

    Take 1: when in advantage then a sacrifice move might be necessary
    to keep the edge
    bench: 5458651

--- src/search.cpp
+++ src/search.cpp
@@ -1030,7 +1030,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
-              if (!pos.see_ge(move, Value((-21 - 7 * (bool)(ss->staticEval - 100 > alpha )) * lmrDepth * lmrDepth - 21 * lmrDepth)))
+              if (!pos.see_ge(move, Value((-21 -7 * (bool)(ss->staticEval - 100 > alpha )) * lmrDepth * lmrDepth - 21 * lmrDepth)))
                   continue;
           }
       }
