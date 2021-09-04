a11eaec7bb7f143be0f96de72a159a6f620a73ae
pb00067 <pb00067@PHXL0356.wp.lan>
2021-09-04 15:50:09 +0200

    Take 2: shallow prune also pawn loosing moves (depth <=2)
    except pawn push discovering checks, don't prune them on qsearch too
    bench:  5103675

--- src/search.cpp
+++ src/search.cpp
@@ -1510,7 +1510,12 @@ moves_loop: // When in check, search starts here
       // Do not search moves with negative SEE values
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
           && !pos.see_ge(move))
-          continue;
+      {
+         bool discoPawnPush = givesCheck && !captureOrPromotion && type_of(pos.moved_piece(move)) == PAWN && bool(pos.blockers_for_king(~pos.side_to_move()) & from_sq(move));
+         if (!discoPawnPush)
+             continue;
+      }
+
 
       // Speculative prefetch as early as possible
       prefetch(TT.first_entry(pos.key_after(move)));
