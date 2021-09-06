66b16c06faf85acaf04c8674e4b0a650d15d1d42
pb00067 <pb00067@PHXL0356.wp.lan>
2021-09-06 09:10:38 +0200

    Take 5: prune loosing pawn pushes and BxN exchanges too (depth <=2)
    bench: 4883067

--- src/search.cpp
+++ src/search.cpp
@@ -1012,7 +1012,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning
-              if (!pos.see_ge(move, Value(depth <= 1 ? -120 : -218) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(depth <= 2 ? KnightValueMg - BishopValueMg : -218) * depth)) // (~25 Elo)
                      continue;
           }
           else
