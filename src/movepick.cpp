e947183ff89b42cbfdcbdb0273eba5d73a9a78a6
pb00067 <pb00067@PHXL0356.wp.lan>
2021-11-16 16:34:18 +0100

    Require smaller value (80) to make BxN trade pass as good capture.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -170,7 +170,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
+                       return (cur->value > 80 && type_of(pos.piece_on(to_sq(*cur))) == KNIGHT && type_of(pos.moved_piece(*cur)) == BISHOP) || pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
