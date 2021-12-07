8ba16fe7b9528f6865b046324a86c60dbc8df064
pb00067 <pb00067@PHXL0356.wp.lan>
2021-12-07 11:03:50 +0100

    Take 1
    bench: 4659136

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -112,7 +112,7 @@ void MovePicker::score() {
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
                    + (ply < MAX_LPH ? 6 * (*lowPlyHistory)[ply][from_to(m)] : 0);
-          if (from_sq(m) ==  recaptureSquare)
+          if (from_sq(m) ==  refutationFrom)
               m.value += 1500;
       }
 
