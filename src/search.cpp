c4b894b2d8a9f531481db47286a5fdc2fd6cc706
pb00067 <pb00067@PHXL0356.wp.lan>
2021-12-13 15:48:58 +0100

    Take 3
    bench: 4390179

--- src/search.cpp
+++ src/search.cpp
@@ -1723,8 +1723,8 @@ moves_loop: // When in check, search starts here
         // Decrease stats for all non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
         {
-            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2 - farBelowBest[i];
-            update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
+            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2 -farBelowBest[i];
+            update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2 -farBelowBest[i]);
         }
     }
     else
