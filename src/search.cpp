31ab590a2d48ebfba4e369fc30ac082dcd009d43
pb00067 <pb00067@PHXL0356.wp.lan>
2021-12-18 00:53:53 +0100

    Simplification try
    bench: 4505524

--- src/search.cpp
+++ src/search.cpp
@@ -696,13 +696,6 @@ namespace {
                 if ((ss-1)->moveCount <= 2 && !priorCapture)
                     update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + 1));
             }
-            // Penalty for a quiet ttMove that fails low
-            else if (!ttCapture)
-            {
-                int penalty = -stat_bonus(depth);
-                thisThread->mainHistory[us][from_to(ttMove)] << penalty;
-                update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
-            }
         }
 
         // Partial workaround for the graph history interaction problem
