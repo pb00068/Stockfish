cb01743871304b45ae7d028627166e7c783f4411
pb00067 <pb00067@PHXL0356.wp.lan>
2022-01-03 10:32:12 +0100

    Take 4
    bench: 4672703

--- src/search.cpp
+++ src/search.cpp
@@ -1545,14 +1545,8 @@ moves_loop: // When in check, search starts here
 
       // Do not search moves with negative SEE values (~5 Elo)
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move))
-      {
-         if (!captureOrPromotion)
+          && !pos.see_ge(move, KnightValueMg - BishopValueMg))
              continue;
-         PieceType captured = type_of(pos.piece_on(to_sq(move)));
-         if (thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][captured] <= 2000)
-             continue;
-      }
 
       // Speculative prefetch as early as possible
       prefetch(TT.first_entry(pos.key_after(move)));
