01db247bc9cd80e0f2963991ac0381f44555f211
pb00067 <pb00067@PHXL0356.wp.lan>
2022-01-03 14:30:47 +0100

    Take 2
    bench: 4452346

--- src/search.cpp
+++ src/search.cpp
@@ -1693,9 +1693,17 @@ moves_loop: // When in check, search starts here
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
     }
-    else
+    else {
         // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
+        if (type_of(moved_piece) > captured && quietCount > 1 && depth > 3) {
+             for (int i = 0; i < quietCount; ++i)
+             {
+                  thisThread->mainHistory[us][from_to(quietsSearched[i])] << -stat_bonus(depth-1);
+                  update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -stat_bonus(depth-1));
+             }
+        }
+    }
 
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
