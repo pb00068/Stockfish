07cc660dad49bd4f5f70e1a958e4a5cf7481721e
pb00067 <pb00067@PHXL0356.wp.lan>
2022-05-17 09:15:00 +0200

    Take 5, doubling the boost
    bench: 6315926

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -135,7 +135,7 @@ void MovePicker::score() {
       if constexpr (Type == CAPTURES) {
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   +     (m.move == captKiller ? 5000 : 0);
+                   +     (m.move == captKiller ? 10000 : 0);
       }
 
       else if constexpr (Type == QUIETS)
