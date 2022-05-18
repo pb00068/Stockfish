749c9f540f87df80657ab8d96719c7124d7abf33
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2022-05-18 14:19:27 +0200

    Currently if a heavy piece moves backwards along a slider threat line
    movepick thinks that this escapes the threat which is not true.

--- src/position.h
+++ src/position.h
@@ -296,7 +296,7 @@ inline Bitboard Position::attacks_by(Color c) const {
       Bitboard threats = 0;
       Bitboard attackers = pieces(c, Pt);
       while (attackers)
-          threats |= attacks_bb<Pt>(pop_lsb(attackers), pieces());
+          threats |= attacks_bb<Pt>(pop_lsb(attackers), pieces() ^ pieces(~c, QUEEN, ROOK));
       return threats;
   }
 }
