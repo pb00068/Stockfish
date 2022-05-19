f78e05522e8882333e86b233c0276d95337790e1
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2022-05-19 10:07:10 +0200

    Take 3, Break usage of follow move histories when intermit by a capture
    bench: 6499983

--- src/search.cpp
+++ src/search.cpp
@@ -942,7 +942,7 @@ moves_loop: // When in check, search starts here
 
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
-    nullptr                                                         , (ss-2)->cmCapture ?  thisThread->sentinel : (ss-4)->continuationHistory,
+    nullptr                                                         , (ss-4)->continuationHistory,
     nullptr                                   ,  (ss-2)->cmCapture || (ss-4)->cmCapture ?  thisThread->sentinel : (ss-6)->continuationHistory };
 
     Move countermove = thisThread->counterMoves[pos.piece_on(prevSq)][prevSq];
@@ -1505,7 +1505,7 @@ moves_loop: // When in check, search starts here
     }
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
-    nullptr                                                         , (ss-2)->cmCapture ?  thisThread->sentinel : (ss-4)->continuationHistory,
+    nullptr                                                         , (ss-4)->continuationHistory,
     nullptr                                   ,  (ss-2)->cmCapture || (ss-4)->cmCapture ?  thisThread->sentinel : (ss-6)->continuationHistory };
     // Initialize a MovePicker object for the current position, and prepare
     // to search the moves. Because the depth is <= 0 here, only captures,
@@ -1750,10 +1750,10 @@ moves_loop: // When in check, search starts here
         // Only update first 2 continuation histories if we are in check
         if (ss->inCheck && i > 2)
             break;
-        if (!dirty && is_ok((ss-i)->currentMove))
+        if ((!dirty || i == 2) && is_ok((ss-i)->currentMove))
         {
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
-            dirty =  (i % 2 == 0) && (ss-i)->cmCapture;
+            dirty |=  (i % 2 == 0) && (ss-i)->cmCapture;
         }
     }
   }
