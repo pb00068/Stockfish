254ab6fcf9289b0d87090534bb4a4a815bf31ebd
pb00067 <pb00067@PHXL0356.wp.lan>
2022-06-01 20:02:57 +0200

    Take 1: the good/bad formula pos.see_ge(*cur, -69 * cur->value / 1024)
    is symmetric but the capture scores are in avg way over 0 (~2570) for
    neutral exchanges (=SEE value zero). Try to compensate it
    
    bench: 6695914

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -134,7 +134,9 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
+                   -     3330 // offset to make neutral exchanges (PAWNS) score about 0
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
