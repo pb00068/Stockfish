767297b99f897a467449f7aaee9bd82960b6e412
pb00067 <pb00067@PHXL0356.wp.lan>
2022-06-24 09:33:49 +0200

    Take 1: fast shallow prune blunder quiete moves
    bench: 4924644

--- src/search.cpp
+++ src/search.cpp
@@ -951,6 +951,7 @@ moves_loop: // When in check, search starts here
                          && ttMove
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
+    Bitboard badSquares = 0;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1039,8 +1040,16 @@ moves_loop: // When in check, search starts here
                   && ss->staticEval + 122 + 138 * lmrDepth + history / 60 <= alpha)
                   continue;
 
+              if (!badSquares) // init attackedByPawn now if not done already
+                   badSquares = pos.attacks_by<PAWN>(~us) | pos.pieces(~us);
+
+              if (type_of(movedPiece) >= KNIGHT && (badSquares & to_sq(move)))
+              {
+                  if (lmrDepth < 1 + type_of(movedPiece))
+                    continue;
+              }
               // Prune moves with negative SEE (~3 Elo)
-              if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
+              else if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
                   continue;
           }
       }
