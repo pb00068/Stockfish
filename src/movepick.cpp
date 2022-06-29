917a4edc8b541d1c3af26b03726d4bf7ea07ca11
pb00067 <pb00067@PHXL0356.wp.lan>
2022-06-29 11:48:38 +0200

    movepick score, take 2: Encourage king to move when causing pins (corrected pharentesis)
    bench:  5382760

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -142,7 +142,7 @@ void MovePicker::score() {
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   +     (type_of(pos.moved_piece(m)) == KING && pos.blockers_for_king(pos.side_to_move())) ? 15000 * popcount(pos.blockers_for_king(pos.side_to_move())): 0
+                   +     (type_of(pos.moved_piece(m)) == KING && pos.blockers_for_king(pos.side_to_move()) ? 15000 * popcount(pos.blockers_for_king(pos.side_to_move())) : 0)
                    +     (threatened & from_sq(m) ?
                            (type_of(pos.moved_piece(m)) == QUEEN && !(to_sq(m) & threatenedByRook)  ? 50000
                           : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
