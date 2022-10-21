cf49da1e26e482d6be78614f641caa964063132a
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2022-10-21 10:19:00 +0200

    Take 2
    bench: 4238813

--- src/search.cpp
+++ src/search.cpp
@@ -1298,9 +1298,10 @@ moves_loop: // When in check, search starts here
           }
       }
       else {
-         ss->cutoffCnt = 0;
-         if (move == countermove && is_ok((ss-1)->currentMove))
+
+         if (move == countermove && !ss->cutoffCnt && is_ok((ss-1)->currentMove))
             thisThread->counterMoves[pos.piece_on(prevSq)][prevSq] = MOVE_NONE;
+         ss->cutoffCnt = 0;
       }
 
       // If the move is worse than some previously searched move, remember it to update its stats later
