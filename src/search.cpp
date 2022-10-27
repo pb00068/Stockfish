1a6803956094e247ea52b1d2a6e3df773a168045
pb00067 <pb00067@PHXL0356.wp.lan>
2022-10-27 11:24:41 +0200

    Trying another idea @ fail low: don't penalize move if contained in
    the PV  (within next 2 follow moves)
    
    bench:  4000155

--- src/search.cpp
+++ src/search.cpp
@@ -1304,17 +1304,15 @@ moves_loop: // When in check, search starts here
       // If the move is worse than some previously searched move, remember it to update its stats later
       if (move != bestMove)
       {
+        if(PvNode && ss->pv[0] != MOVE_NONE && ss->pv[1] != MOVE_NONE &&
+          (ss->pv[2] == move || (ss->pv[2] != MOVE_NONE && ss->pv[3] != MOVE_NONE && ss->pv[4] == move)))
+           {   }
+        else {
           if (capture && captureCount < 32)
               capturesSearched[captureCount++] = move;
-
-          else if (!capture && quietCount < 64) {
+          else if (!capture && quietCount < 64)
               quietsSearched[quietCount++] = move;
-              if(PvNode && value == alpha && ss->pv[0] != MOVE_NONE && ss->pv[1] != MOVE_NONE &&
-              (ss->pv[2] == move || (ss->pv[2] != MOVE_NONE && ss->pv[3] != MOVE_NONE && ss->pv[4] == move)))
-              {
-                 quietCount--; // we assume here a transposition thus move & bestMove equally strong
-              }
-          }
+        }
       }
     }
 
