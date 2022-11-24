5db06affddebf6256e79991ae9de948771e70cdc
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2022-11-24 15:07:08 +0100

    Thread voting: do average of searched- & completed-depth
    Downgrade upperbound scores by 18
    bench: 4390318

--- src/thread.cpp
+++ src/thread.cpp
@@ -224,7 +224,8 @@ Thread* ThreadPool::get_best_thread() const {
     for (Thread* th : *this)
     {
         votes[th->rootMoves[0].pv[0]] +=
-            (th->rootMoves[0].score - minScore + 14) * int(th->completedDepth);
+            (th->rootMoves[0].score - minScore + 14 - th->rootMoves[0].scoreUpperbound * 18) *
+              int((th->completedDepth + th->rootMoves[0].searchedDepth)/2);
 
         if (abs(bestThread->rootMoves[0].score) >= VALUE_TB_WIN_IN_MAX_PLY)
         {
