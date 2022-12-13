f6d78a907b63246b7d66a3615855a7c0fe6c6342
pb00067 <pb00067@PHXL0356.wp.lan>
2022-12-13 09:29:07 +0100

    qsearch: Allow TT cut-offs on PV too when tt->depth is ensurand
    bench: 3616852

--- src/search.cpp
+++ src/search.cpp
@@ -1439,7 +1439,7 @@ moves_loop: // When in check, search starts here
     ttMove = ss->ttHit ? tte->move() : MOVE_NONE;
     pvHit = ss->ttHit && tte->is_pv();
 
-    if (  !PvNode
+    if (  (!PvNode || tte->depth() > ttDepth + 1)
         && ss->ttHit
         && tte->depth() >= ttDepth
         && ttValue != VALUE_NONE // Only in case of TT access race
