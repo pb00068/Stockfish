c29debf6eef61182ce08dc10ebc8c11c07277551
pb00067 <pb00067@PHXL0356.wp.lan>
2022-12-15 09:16:50 +0100

    Take 2: simplier approach, no votes.
    Just consider most recent valid result for choosing bestmove.
    bench: 3410998

--- src/thread.cpp
+++ src/thread.cpp
@@ -215,28 +215,24 @@ void ThreadPool::start_thinking(Position& pos, StateListPtr& states,
 Thread* ThreadPool::get_best_thread() const {
 
     Thread* bestThread = front();
-    std::map<Move, int64_t> votes;
-    Value minScore = VALUE_NONE;
-    int oldestRun = 2147483647;
-    int mostRecentRun = 1;
 
-    // Find minimum score of all threads
-    for (Thread* th: *this) {
-        minScore = std::min(minScore, th->rootMoves[0].score);
-        oldestRun = std::min(oldestRun, th->rootMoves[0].aspSearch);
-        mostRecentRun = std::max(mostRecentRun, th->rootMoves[0].aspSearch);
+
+    // Find thread with most recent result (most recent aspiration search run)
+    for (Thread* th: *this)
+    {
+       // exclude fail lows, because here pv[0] is from previous iteration
+       if (th->rootMoves[0].scoreUpperbound)
+         continue;
+       if (th->rootMoves[0].aspSearch > bestThread->rootMoves[0].aspSearch ||
+          (th->rootMoves[0].aspSearch == bestThread->rootMoves[0].aspSearch && th->rootMoves[0].score > bestThread->rootMoves[0].score))
+            bestThread = th;
     }
-    int distance = 1 + mostRecentRun - oldestRun;
 
-    // Vote according to score and most recent search, and select the best thread
-    auto thread_value = [minScore, oldestRun, distance](Thread* th) {
-            return (th->rootMoves[0].score - minScore + 14) * int((15 * (th->rootMoves[0].aspSearch - oldestRun)) / distance);
-        };
-
-    for (Thread* th : *this)
-        votes[th->rootMoves[0].pv[0]] += thread_value(th);
-
-    for (Thread* th : *this)
+    Move bestMove = bestThread->rootMoves[0].pv[0];
+    // Among threads with this move, choose the one with the shortest Mate, otherwise longest PV
+    for (Thread* th : *this) {
+        if (th->rootMoves[0].pv[0] != bestMove)
+           continue;
         if (abs(bestThread->rootMoves[0].score) >= VALUE_TB_WIN_IN_MAX_PLY)
         {
             // Make sure we pick the shortest mate / TB conversion or stave off mate the longest
@@ -245,11 +241,9 @@ Thread* ThreadPool::get_best_thread() const {
         }
         else if (   th->rootMoves[0].score >= VALUE_TB_WIN_IN_MAX_PLY
                  || (   th->rootMoves[0].score > VALUE_TB_LOSS_IN_MAX_PLY
-                     && (   votes[th->rootMoves[0].pv[0]] > votes[bestThread->rootMoves[0].pv[0]]
-                         || (   votes[th->rootMoves[0].pv[0]] == votes[bestThread->rootMoves[0].pv[0]]
-                             &&   thread_value(th) * int(th->rootMoves[0].pv.size() > 2)
-                                > thread_value(bestThread) * int(bestThread->rootMoves[0].pv.size() > 2)))))
+                     && th->rootMoves[0].pv.size() > bestThread->rootMoves[0].pv.size()))
             bestThread = th;
+    }
 
     return bestThread;
 }
