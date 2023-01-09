efcc675d43d5f1e206f82db85504e433d4b541b7
pb00067 <pb00067@PHXL0356.wp.lan>
2023-01-09 16:00:56 +0100

    Take 1
    bench: 4429081

--- src/search.cpp
+++ src/search.cpp
@@ -1227,7 +1227,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv[0] = MOVE_NONE;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha,
-                              std::min(maxNextDepth, newDepth), false);
+                              std::min(maxNextDepth, newDepth), rootNode && value >= beta);
       }
 
       // Step 19. Undo move
