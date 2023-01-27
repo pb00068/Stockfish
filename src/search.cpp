781b6cd72b7359bbb99d6fbb026dc4f9bec80aac
pb00067 <pb00067@PHXL0356.wp.lan>
2023-01-27 12:05:36 +0100

    Take 2: trying to discover the mystery.
    According local tests this little (non-sense)change makes it elo-neutral
    bench: 3788031

--- src/search.cpp
+++ src/search.cpp
@@ -1066,6 +1066,7 @@ moves_loop: // When in check, search starts here
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
+              thisThread->complexityAverage.update(complexity);
               value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
               ss->excludedMove = MOVE_NONE;
 
