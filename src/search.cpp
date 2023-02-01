5ced832fd58901b04b9a2639cd6504f3e4c71dc7
pb00067 <pb00067@PHXL0356.wp.lan>
2023-02-01 09:22:15 +0100

    Take 2: trying to improve on struggling https://tests.stockfishchess.org/tests/view/63d4ca6abde6e5f3cb4bf726 Don't even probe TT with excluded move set, use stack to inherit tte.
    bench: 3775551

--- src/search.cpp
+++ src/search.cpp
@@ -1089,7 +1089,6 @@ moves_loop: // When in check, search starts here
               if (value < singularBeta)
               {
                   extension = 1;
-                  eval = ss->staticEval = evaluate(pos, &complexity);
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
