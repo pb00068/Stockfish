3adb4bed4074e67169df121c077000e70ab69227
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-02-07 19:01:35 +0100

    Do we still loose 13 Elo? Do the recalc not in the SE node with exlcudedMove but int the enclosing search as soon it returns from the reduced search. Fixed games, very short tc.
    bench: 3942006

--- src/search.cpp
+++ src/search.cpp
@@ -731,9 +731,7 @@ namespace {
     }
     else if (excludedMove) {
         // excludeMove implies that we had a ttHit on the containing non-excluded search with ss->staticEval filled from TT
-        // However static evals from the TT aren't good enough (-13 elo), presumably due to changing optimism context
-        // Recalculate value with current optimism (without updating thread avgComplexity)
-        ss->staticEval = eval = evaluate(pos, &complexity);
+        eval = ss->staticEval;
     }
     else if (ss->ttHit)
     {
@@ -1068,8 +1066,8 @@ moves_loop: // When in check, search starts here
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
-              // the search with excludedMove will update ss->staticEval
               value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
+              ss->staticEval = eval = evaluate(pos, &complexity);
               ss->excludedMove = MOVE_NONE;
 
               if (value < singularBeta)
