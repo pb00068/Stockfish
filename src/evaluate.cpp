2a0ec23ebfe114e29b24eb7242dd6d7b70f00c25
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-02-10 14:53:12 +0100

    Revert "Take 1: damp down logic seems to be the main cause for eval differences"
    This reverts commit 46647c5fca9eb03f950f173a9dbe18c05e73b72c.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1085,7 +1085,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (200 - std::max(40, pos.rule50_count()) ) / 174;
+  v = v * (200 - pos.rule50_count()) / 214;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
