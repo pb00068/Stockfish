5d2dc098c674ffa1208f6bba23c49b5a988d0598
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-02-14 14:25:37 +0100

    Trust TT-Entry on SE-node with excludedMove
    bench: 2618037

--- src/search.cpp
+++ src/search.cpp
@@ -738,7 +738,10 @@ namespace {
         // Never assume anything about values stored in TT
         ss->staticEval = eval = tte->eval();
         if (eval == VALUE_NONE)
+        {
             ss->staticEval = eval = evaluate(pos, &complexity);
+            thisThread->complexityAverage.update(complexity);
+        }
         else // Fall back to (semi)classical complexity for TT hits, the NNUE complexity is lost
             complexity = abs(ss->staticEval - pos.psq_eg_stm());
 
@@ -750,10 +753,11 @@ namespace {
     else
     {
         ss->staticEval = eval = evaluate(pos, &complexity);
+        thisThread->complexityAverage.update(complexity);
         // Save static evaluation into transposition table
         tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
     }
-    thisThread->complexityAverage.update(complexity);
+
 
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture)
