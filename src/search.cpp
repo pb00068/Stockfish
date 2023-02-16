5467cfda8ba2f66f634ac703553cfe672c0b5325
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-02-16 14:04:49 +0100

    Take 4: do only the essential and avoid useless classical recalcs.
    Run with normal bounds
    
    bench: 4119690

--- src/search.cpp
+++ src/search.cpp
@@ -731,9 +731,9 @@ namespace {
     }
     else if (excludedMove) {
         // excludeMove implies that we had a ttHit on the containing non-excluded search with ss->staticEval filled from TT
-        // not that search with excluded move uses same ply (is done with ss instead to ss+1)
+        // because this search is called on same ply (ss instead to ss+1).
         eval = ss->staticEval; // trust tt-entry
-        if (Eval::useNNUE)
+        if (Eval::useNNUE && ((pos.count<ALL_PIECES>() <= 7 || abs(pos.psq_eg_stm()) <= 1781)))
             Eval::NNUE::evaluate(pos, true, nullptr);
     }
     else if (ss->ttHit)
