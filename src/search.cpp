06cf99e3d94612c4ffc16fbb1dd80dda933143e7
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-02-22 10:55:50 +0100

    Also Quiet ttMove extensions are nodes with millions of children.
    This nodes are much rarer than SE-nodes but maybe it's a measurable
    speedup?
    bench: 4283297

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,11 @@ moves_loop: // When in check, search starts here
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5600)
+          {
               extension = 1;
+              if (Eval::useNNUE)
+                 Eval::NNUE::evaluate(pos, true, nullptr);
+          }
       }
 
       // Add extension to new depth
