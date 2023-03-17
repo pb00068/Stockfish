9e721e6f9a32a3589847368ae1c2902bc610e6d3
pb00067 <pb00067@phxl0401.wp.lan>
2023-03-17 09:41:53 +0100

    Take 1
    bench: 5013350

--- src/search.cpp
+++ src/search.cpp
@@ -952,6 +952,7 @@ moves_loop: // When in check, search starts here
                          && ttMove
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
+    int triedEscapes = 0;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1001,7 +1002,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount + 2 * triedEscapes >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - r, 0);
@@ -1171,9 +1172,12 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Decrease reduction if we move a threatened piece (~1 Elo)
-      if (   depth > 9
+      if (   depth > 4
           && (mp.threatenedPieces & from_sq(move)))
+      {
           r--;
+          triedEscapes++;
+      }
 
       // Increase reduction if next ply has a lot of fail high
       if ((ss+1)->cutoffCnt > 3)
