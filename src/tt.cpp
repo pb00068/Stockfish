5c8bba381f0efb48bad6c7e0f9e9e0ff73a9d99a
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-03-24 11:18:03 +0100

    Take 2 TT: adjust override logic to pvDistance
    bench: 4720306

--- src/tt.cpp
+++ src/tt.cpp
@@ -33,7 +33,7 @@ TranspositionTable TT; // Our global transposition table
 /// TTEntry::save() populates the TTEntry with a new node's data, possibly
 /// overwriting an old position. Update is not atomic and can be racy.
 
-void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) {
+void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, int depthOffset) {
 
   // Preserve any existing move for the same position
   if (m || (uint16_t)k != key16)
@@ -42,7 +42,7 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
   // Overwrite less valuable entries (cheapest checks first)
   if (   b == BOUND_EXACT
       || (uint16_t)k != key16
-      || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
+      || d - DEPTH_OFFSET + 2 * pv > depth8 - depthOffset)
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
--- src/tt.h
+++ src/tt.h
@@ -43,7 +43,7 @@ struct TTEntry {
   Depth depth() const { return (Depth)depth8 + DEPTH_OFFSET; }
   bool is_pv()  const { return (bool)(genBound8 & 0x4); }
   Bound bound() const { return (Bound)(genBound8 & 0x3); }
-  void save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev);
+  void save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, int depthOffset);
 
 private:
   friend class TranspositionTable;
