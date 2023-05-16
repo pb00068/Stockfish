917e036ac53fcea99dc6c4495ed62eb53d93aeb5
pb00067 <pb00067@phxl0401.wp.lan>
2023-05-16 12:27:29 +0200

    Take 1
    bench: 3625410

--- src/search.cpp
+++ src/search.cpp
@@ -1027,16 +1027,13 @@ moves_loop: // When in check, search starts here
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth += history / 7278;
-              lmrDepth = std::max(lmrDepth, -2);
+              int lmr = std::max(lmrDepth + history / 7278, -2);
 
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
-                  && lmrDepth < 13
-                  && ss->staticEval + 103 + 138 * lmrDepth <= alpha)
+                  && lmr < 13
+                  && ss->staticEval + 103 + 138 * lmr <= alpha)
                   continue;
-
-              lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
               if (!pos.see_ge(move, Value(-24 * lmrDepth * lmrDepth - 16 * lmrDepth)))
