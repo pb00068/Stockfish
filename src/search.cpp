db721af6691a6617999826eb626962c289a44269
pb00067 <pb00067@phxl0401.wp.lan>
2023-07-26 10:05:40 +0200

    Take 2
    bench: 1627332

--- src/search.cpp
+++ src/search.cpp
@@ -1715,11 +1715,11 @@ moves_loop: // When in check, search starts here
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bestMoveBonus);
         }
     }
-    else
+    else if (!ss->ttHit || captureCount)
     {
         // Increase stats for the best move in case it was a capture move
         captured = type_of(pos.piece_on(to_sq(bestMove)));
-        captureHistory[moved_piece][to_sq(bestMove)][captured] << ((ss->ttHit && !captureCount && !quietCount) ? stat_bonus(depth) : quietMoveBonus);
+        captureHistory[moved_piece][to_sq(bestMove)][captured] << quietMoveBonus;
     }
 
     // Extra penalty for a quiet early move that was not a TT move or
