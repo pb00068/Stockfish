76704cb55c14939e66a6b8dd8aa0813e42c07a8c
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-08-24 10:03:32 +0200

    Take 1: null move ttvalue experiment
    bench: 1695953

--- src/search.cpp
+++ src/search.cpp
@@ -1472,7 +1472,11 @@ moves_loop: // When in check, search starts here
             // ttValue can be used as a better position evaluation (~13 Elo)
             if (    ttValue != VALUE_NONE
                 && (tte->bound() & (ttValue > bestValue ? BOUND_LOWER : BOUND_UPPER)))
+            {
                 bestValue = ttValue;
+                if ((ss-1)->currentMove == MOVE_NULL)
+                    (ss-1)->staticEval = -bestValue;
+            }
         }
         else
             // In case of null move search use previous static eval with a different sign
