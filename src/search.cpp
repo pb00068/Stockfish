cac9bc9716bb514e63172f41b17a92d92ea79776
pb00067 <pb00067@phxl0401.wp.lan>
2023-09-21 08:43:16 +0200

    Take 4: only extend subsequent checks but exclude repeated identical check moves
    bench: 1330996

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,8 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth >6 && ((ss-1)->inCheck || (ss-3)->inCheck || (ss-5)->inCheck))
+                   && depth > 8 &&
+                  ((ss-1)->inCheck || ((ss-3)->inCheck && (ss-4)->currentMove != move)  || ((ss-5)->inCheck && (ss-6)->currentMove != move)))
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
