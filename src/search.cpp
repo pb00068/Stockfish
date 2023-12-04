842e8e688a74ae3e60b4e19818a59792716d0bda
pb00067 <pb00067@phxl0401.wp.lan>
2023-12-04 10:00:41 +0100

    Take 3
    bench: 1485015

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop:  // When in check, search starts here
                     singularQuietLMR = !ttCapture;
 
                     // Avoid search explosion by limiting the number of double extensions
-                    if (!PvNode && value < singularBeta - ss->moveCount - 11 && ss->doubleExtensions <= 11)
+                    if (!PvNode && value < singularBeta - ss->moveCount/2 - 14 && ss->doubleExtensions <= 11)
                     {
                         extension = 2;
                         depth += depth < 15;
