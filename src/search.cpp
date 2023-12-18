361e74f1060ce3f3005ae6f27ac6137d78b7b96a
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2023-12-18 09:41:49 +0100

    Take 1
    bench: 1617981

--- src/search.cpp
+++ src/search.cpp
@@ -923,7 +923,7 @@ moves_loop:  // When in check, search starts here
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal to or greater than the current depth, and the result
     // of this search was a fail low.
-    bool likelyFailLow = PvNode && ttMove && (tte->bound() & BOUND_UPPER) && tte->depth() >= depth;
+    bool likelyFailLow = PvNode && ttMove && (tte->bound() & BOUND_UPPER) && tte->depth() >= depth && tte->value() <= alpha;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
