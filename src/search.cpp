7004a6edb4574b0ea706fda079fe02ac98ebf8ad
pb00067 <pb00067@phxl0401.wp.lan>
2024-03-15 16:29:55 +0100

    Another approach
    bench: 1762901

--- src/search.cpp
+++ src/search.cpp
@@ -775,15 +775,17 @@ Value Search::Worker::search(
     {
         assert(eval - beta >= 0);
 
-        // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 151, 6) + depth / 3 + 4;
+        // Null move depth based on depth and eval
+        Depth nmDepth = 2 * depth / 3 - std::min(int(eval - beta) / 151, 6) - 4;
+        if (abs(beta) < 40 && thisThread->rootDepth - ss->ply > 10)
+           nmDepth += (thisThread->rootDepth - ss->ply) / 3;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
 
         pos.do_null_move(st, tt);
 
-        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, !cutNode);
+        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, nmDepth, !cutNode);
 
         pos.undo_null_move();
 
@@ -797,9 +799,9 @@ Value Search::Worker::search(
 
             // Do verification search at high depths, with null move pruning disabled
             // until ply exceeds nmpMinPly.
-            thisThread->nmpMinPly = ss->ply + 3 * (depth - R) / 4;
+            thisThread->nmpMinPly = ss->ply + 3 * nmDepth / 4;
 
-            Value v = search<NonPV>(pos, ss, beta - 1, beta, depth - R, false);
+            Value v = search<NonPV>(pos, ss, beta - 1, beta, nmDepth, false);
 
             thisThread->nmpMinPly = 0;
 
