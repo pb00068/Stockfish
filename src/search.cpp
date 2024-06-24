4b51c59ac84ca8da8d458c2564b2f350b50bd670
pb00067 <guenther.demetz@wuerth-phoenix.com>
2024-06-24 15:28:31 +0200

    Take 1: on subsequent fail high raise alpha too
    bench: 1188594

--- src/search.cpp
+++ src/search.cpp
@@ -368,6 +368,8 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
+                    if (failedHighCnt) // on subsequent fail high raise alpha too
+                        alpha  = (alpha + beta) / 2;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
