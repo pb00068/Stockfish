69764b4a1ea47f5e140112043af5728af8fda120
pb00067 <guenther.demetz@wuerth-phoenix.com>
2024-06-25 05:58:12 +0200

    Take 2: increased effect with ongoing FH's
    bench: 1228441

--- src/search.cpp
+++ src/search.cpp
@@ -369,7 +369,7 @@ void Search::Worker::iterative_deepening() {
                 else if (bestValue >= beta)
                 {
                     if (failedHighCnt) // on subsequent fail high raise alpha too
-                        alpha  = (alpha + beta) / 2;
+                        alpha  = (alpha + failedHighCnt * beta) / (failedHighCnt + 1);
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
