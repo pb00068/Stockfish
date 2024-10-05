5f9281b75becf08417e27a1807ff0f5809d5f87a
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2024-10-05 18:34:17 +0200

    Take 3, rebased to current master,
    LTC for https://tests.stockfishchess.org/tests/view/66febe2086d5ee47d953bb33
    
    bench: 1181035

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -164,7 +164,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 
-            // bonus for checks
+            // bonus for checks (direct checks and some special discovering check too)
             m.value += bool(pos.check_squares(pt) & to) * 16384;
 
             // bonus for escaping from capture
