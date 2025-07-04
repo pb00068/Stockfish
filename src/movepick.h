f60020389545d30ff5e84e8a465c8e03ea2e5f9d
pb00067 <guenther.demetz@wuerth-phoenix.com>
2025-07-04 10:50:18 +0200

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I464bc6e487879a301f062a7607c9a98765bb81ae

--- src/movepick.h
+++ src/movepick.h
@@ -50,7 +50,7 @@ class MovePicker {
     MovePicker(const Position&, Move, int, const CapturePieceToHistory*);
     Move next_move();
     void skip_quiet_moves();
-    bool can_move_king_or_pawn() const;
+    bool non_desperado_mobile(Square sq);
 
    private:
     template<typename Pred>
