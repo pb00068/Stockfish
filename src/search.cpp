cfcf706747f9bf31fa7b1455865d2b1b707b47ed
pb00067 <guenther.demetz@wuerth-phoenix.com>
2025-09-08 16:50:17 +0200

    My try to simplify away verification using the double null move trick
    (without loosing performance on extended zugwang allgauer suite)
    
    bench: 3266874

--- src/search.cpp
+++ src/search.cpp
@@ -872,43 +872,28 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval >= beta - 18 * depth + 390 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
+    if ((cutNode && ss->staticEval >= beta - 18 * depth + 390 && !excludedMove
+        && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta)) || ((ss-1)->currentMove == Move::null() && !cutNode))
     {
-        assert((ss - 1)->currentMove != Move::null());
-
         // Null move dynamic reduction based on depth
-        Depth R = 6 + depth / 3;
+        Depth R = cutNode ? 6 + depth / 3 : 0;
 
         ss->currentMove                   = Move::null();
         ss->continuationHistory           = &continuationHistory[0][0][NO_PIECE][0];
         ss->continuationCorrectionHistory = &continuationCorrectionHistory[NO_PIECE][0];
 
         do_null_move(pos, st);
+        if (!nmpMinPly)
+             nmpMinPly = ss->ply + 3 * (depth - R) / 4;
 
-        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
+        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, !cutNode);
 
+        nmpMinPly = 0;
         undo_null_move(pos);
 
         // Do not return unproven mate or TB scores
-        if (nullValue >= beta && !is_win(nullValue))
-        {
-            if (nmpMinPly || depth < 16)
-                return nullValue;
-
-            assert(!nmpMinPly);  // Recursive verification is not allowed
-
-            // Do verification search at high depths, with null move pruning disabled
-            // until ply exceeds nmpMinPly.
-            nmpMinPly = ss->ply + 3 * (depth - R) / 4;
-
-            Value v = search<NonPV>(pos, ss, beta - 1, beta, depth - R, false);
-
-            nmpMinPly = 0;
-
-            if (v >= beta)
-                return nullValue;
-        }
+        if (nullValue >= beta)
+           return nullValue;
     }
 
     improving |= ss->staticEval >= beta;
