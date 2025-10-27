040c2235331a77130c77f3133b634d682c2e9b33
pb00067 <guenther.demetz@wuerth-phoenix.com>
2025-10-27 16:12:32 +0100

    Revert "Take 12: experimental (will either work or fail fast)"
    This reverts commit 5ed1406de232f5608d24de321dc047a23ec501db.

--- src/search.h
+++ src/search.h
@@ -305,11 +305,11 @@ class Worker {
 
     // This is the main search function, for both PV and non-PV nodes
     template<NodeType nodeType>
-    Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, int distance2Mate);
+    Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, bool cutNode);
 
     // Quiescence search function, which is called by the main search
     template<NodeType nodeType>
-    Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, int distance2Mate);
+    Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta);
 
     Depth reduction(bool i, Depth d, int mn, int delta) const;
 
