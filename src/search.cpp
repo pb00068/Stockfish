d1f81da29f0102de5c3bbe6ad67f3344d392733f
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2025-11-27 16:10:15 +0100

    Take 1
    bench: 3124494

--- src/search.cpp
+++ src/search.cpp
@@ -1857,7 +1857,7 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
     for (const auto [i, weight] : conthist_bonuses)
     {
         // Only update the first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 2)
+        if ((ss->inCheck || (ss-1)->inCheck) && i > 2)
             break;
         if (((ss - i)->currentMove).is_ok())
             (*(ss - i)->continuationHistory)[pc][to] << (bonus * weight / 1024) + 88 * (i < 2);
