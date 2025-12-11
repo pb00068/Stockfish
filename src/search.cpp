259357f1be62ceb8a975a60297e32ab948429a78
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2025-12-11 08:43:41 +0100

    Take 1: Decrease reduction on successful Queen promotions
     at (reduced) null-move subtrees
    
    bench: 2912398

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         // Step 17. Late moves reduction / extension (LMR)
-        if (depth >= 2 && moveCount > 1 && move.promotion_type() != QUEEN)
+        if (depth >= 2 && moveCount > 1)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
